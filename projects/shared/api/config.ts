979dfb1482570991d062b491ffc6281c67c6e37f
liuqi6908 <liuqi6908@163.com>
2023-12-18 15:35:35 +0800

    chore(shared): 修改全局配置相关接口类型定义

--- projects/shared/api/config.ts
+++ projects/shared/api/config.ts
@@ -1,4 +1,4 @@
-import type { IExportConfigDto, IUploadWorkConfigDto, IUpsertConfigBodyDto } from 'zjf-types'
+import type { SysConfig, IConfigDto, IUpsertConfigBodyDto } from 'zjf-types'
 import { useRequest } from '../composables'
 
 const { $get, $post } = useRequest()
@@ -7,12 +7,12 @@ const { $get, $post } = useRequest()
  * 创建或更新全局配置
  */
 export function upsertConfigApi(body: IUpsertConfigBodyDto) {
-  return $post<IExportConfigDto & IUploadWorkConfigDto>('/config', body)
+  return $post<IConfigDto[SysConfig]>('/config', body)
 }
 
 /**
  * 获取指定全局配置
  */
-export function getConfigApi(version: string) {
-  return $get<IExportConfigDto & IUploadWorkConfigDto>(`/config/${version}`)
+export function getConfigApi<T extends SysConfig>(version: T) {
+  return $get<IConfigDto[T]>(`/config/${version}`)
 }
