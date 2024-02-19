18141546b1bb137595ec631c58b4c4854d7febac
liuqi <liuqi6908@163.com>
2024-02-19 17:06:49 +0800

    chore(gateway): 调整分配云桌面发送邮件模版

--- projects/gateway/src/utils/html/templates/desktop-assigned.ts
+++ projects/gateway/src/utils/html/templates/desktop-assigned.ts
@@ -7,30 +7,19 @@ import { header } from '../blocks/header'
 import { DANGER } from '../assets/color'
 import footer from '../blocks/footer'
 
-function importantInfo(key: string, value: string) {
-  return HtmlTag.create('div').appendChild(
-    HtmlTag.create('span').text(`${key}: `).style({ fontWeight: '500' }),
-    HtmlTag.create('span').text(`${value}`).style({
-      background: DANGER,
-      padding: '2px 4px',
-      borderRadius: '4px',
-      fontWeight: '500',
-      color: '#fff',
-    }),
-  )
-}
-
-export function getDesktopConnectInfo(desktop: Desktop) {
-  const { accessUrl, password, account } = desktop
+export function getDesktopConnectInfo() {
   return HtmlTag
     .create('div')
     .appendChild(
-      importantInfo('云桌面登录地址', accessUrl).indent(),
-      importantInfo('云桌面登录账号', account).indent(),
-      importantInfo('云桌面初始密码', password).indent(),
-
+      HtmlTag
+        .create('div')
+        .indent()
+        .appendChild(
+          HtmlTag.create('span').text('请在'),
+          HtmlTag.create('span').text('【用户中心/我的桌面】').color(DANGER).style({ fontWeight: '500' }),
+          HtmlTag.create('span').text('中查看登录信息。'),
+        ),
       HtmlTag.create('div').indent().text(`登录云桌面的使用教程请查看${SysConfigService.appName}的常见问题解答板块。`),
-      HtmlTag.create('div').indent().text('请务必保管好上述信息，以防外泄，若他人知悉您的账号密码，将有权登录并查看该账号上所有信息！').color(DANGER).style({ fontWeight: '500' }),
     )
 }
 
@@ -48,7 +37,7 @@ export function getDesktopAssignedHTML(desktop: Desktop) {
           HtmlTag.create('span').text(`【${getUserName(desktop.user)}】`).color(DANGER).style({ fontWeight: '500' }),
           HtmlTag.create('span').text('您提交的云桌面申请已经通过，管理员已经为您分配好云桌面。'),
         ),
-      getDesktopConnectInfo(desktop),
+      getDesktopConnectInfo(),
       HtmlTag.create('div').indent().text('如有其他疑问，可微信扫码联系客服！'),
       footer,
     )
